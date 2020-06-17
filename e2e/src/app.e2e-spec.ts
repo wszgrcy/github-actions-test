9e8a04c7876f4560049ac9d3c0c479bb8d1ea0f8
chen <wszgrcy@gmail.com>
2020-06-17 09:32:41 +0800

    1

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('github-actions app is running!');
+    expect(page.getTitleText()).toEqual('2020版本');
   });
 
   afterEach(async () => {
