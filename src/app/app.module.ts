70b7ac17cbfff6aa7fb0cdc9f03b887e9750dafc
chen <wszgrcy@gmail.com>
2020-06-17 09:47:09 +0800

    增加路由,查看打包大小

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { RouterModule } from '@angular/router';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot([])],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
